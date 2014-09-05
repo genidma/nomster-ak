175ebf1403b5228554a00157efbd8466c2f91c0f
Adeel Khan <adeelnkhan@gmail.com>
2014-09-05 08:15:43 +0000

    I'm removing the variable for pagination from the en.yml fiile, till I get answers on stack overflow. Also changing the per page pagination display from 2 to 4 in the places_controller.rb file. Heroku is throwing errors when I try to add more records in the heroky console. i.e More then two records. This is why I am making these changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 
-		@places = Place.order("name").page(params[:page]).per(2)
+		@places = Place.order("name").page(params[:page]).per(4)
 
 	end
 end
