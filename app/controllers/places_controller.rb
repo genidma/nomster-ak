7275d45e7b788eb8160fd0996d47fa2f8df11360
Adeel Khan <adeelnkhan@gmail.com>
2014-09-13 12:19:36 +0000

    Locking down the updates of places to only users who created the place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 	
 	def index
 		@places = Place.all
@@ -35,6 +35,9 @@ class PlacesController < ApplicationController
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
