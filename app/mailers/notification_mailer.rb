c16cfb40f3dc8e5c035c077e20305c16a213e122
Adeel Khan <adeelnkhan@gmail.com>
2014-09-29 20:30:43 +0000

    Saving work and taking another look at chapter 32

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-
+  def comment_added(comment)
   	@place = comment.place
   	@place_owner = @place.user
   	mail(to: @place_owner.email,
